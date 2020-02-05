f5d4ef25970f7afc874fbf0f63c1af5bd5db6c2f
kuro <dirkreher@ymail.com>
2020-02-05 11:17:50 +0100

    implemented base methods

--- src/main/java/game/logic/PacManGame.java
+++ src/main/java/game/logic/PacManGame.java
@@ -1,9 +1,51 @@
 package game.logic;
 
+import game.logic.actor.Ghost;
+import game.logic.actor.Pac;
+import game.logic.area.map.MapReader;
+import game.logic.area.map.PlayField;
+import game.logic.exception.InvalidMapException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class PacManGame {
     private final int lives;
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+    private PlayField field;
 
     public PacManGame(int lives) {
         this.lives = lives;
+    }
+
+    public void startNew(File file) {
+        try {
+            final MapReader reader = new MapReader(new FileInputStream(file));
+            field = reader.read();
+            for (Ghost ghost : field.getGhosts()) {
+                executor.execute(ghost);
+            }
+        } catch (FileNotFoundException e) {
+            throw new InvalidMapException(e.getLocalizedMessage());
+        }
+    }
+
+    public Pac getPacMan() {
+        return field.getPacMan();
+    }
+
+    public Ghost[] getGhosts() {
+        return field.getGhosts();
+    }
+
+    public PlayField getField() {
+        return field;
+    }
+
+    public void shutdown() {
+        executor.shutdown();
     }
 }
