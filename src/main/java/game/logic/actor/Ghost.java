0f500477117a2b308200c5f4cef39d27f836848e
kuro <dirkreher@ymail.com>
2020-02-05 11:18:39 +0100

    small API change to AI handling

--- src/main/java/game/logic/actor/Ghost.java
+++ src/main/java/game/logic/actor/Ghost.java
@@ -1,5 +1,7 @@
 package game.logic.actor;
 
+import game.logic.actor.ghostAI.GhostAI;
+import game.logic.actor.ghostAI.StandardAI;
 import game.logic.area.field.Tile;
 import game.logic.area.position.Coordinate;
 
@@ -7,12 +9,34 @@ import java.util.ArrayList;
 
 public class Ghost extends Actor implements Runnable {
 
+    private GhostAI ai;
+    private Pac pacMan;
+
     public Ghost(Coordinate coordinate, ArrayList<Tile> tiles) {
+        this(coordinate, tiles, StandardAI.INSTANCE);
+    }
+
+    public Ghost(Coordinate coordinate, ArrayList<Tile> tiles, GhostAI ai) {
         super(coordinate, tiles);
+        this.ai = ai;
     }
 
     @Override
     public void run() {
+        assert pacMan != null;
 
+    }
+
+    public void setAi(GhostAI ai) {
+        this.ai = ai;
+    }
+
+    public void setPac(Pac pacMan) {
+
+        this.pacMan = pacMan;
+    }
+
+    private boolean pacManDetected() {
+        return this.coordinate.equals(pacMan.coordinate);
     }
 }
