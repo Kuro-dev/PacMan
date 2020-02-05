2d65ffd2c6a99bf00547201802cb4eec5fc9ee18
kuro <dirkreher@ymail.com>
2020-02-05 11:23:06 +0100

    added javadoc and implemented basic 'move' method

--- src/main/java/game/logic/actor/Actor.java
+++ src/main/java/game/logic/actor/Actor.java
@@ -20,18 +20,34 @@ public abstract class Actor {
     }
 
     /**
-     * Moves the actor to the given position, however checks if the position is free beforehand.
+     * Moves the actor to the given position, if possible. Character will not move if not.
      *
-     * @param xOffset    The x offset of which the actor is to be moved
-     * @param yOffset    The y offset of which the actor is to be moved
+     * @param xOffset The x offset of which the actor is to be moved
+     * @param yOffset The y offset of which the actor is to be moved
      */
     public void moveSafely(int xOffset, int yOffset) {
-        final Coordinate dest = this.coordinate.clone();
+        final Coordinate dest = this.coordinate.copy();
         dest.move(xOffset, yOffset);
         boolean peek = peek(dest);
         if (peek) {
-            this.coordinate.move(xOffset, yOffset);
+            move(xOffset, yOffset);
         }
+    }
+
+    public void move(Direction direction) {
+        move(direction.getX(), direction.getY());
+    }
+
+    /**
+     * Moves the actors position by the given offset.
+     * <p>Care should be taken when invoking this method as it does not check whether or not it is safe to move.</p>
+     * <p>In most cases {@link #moveSafely(Direction)} or {@link #moveSafely(int, int)} should be used instead</p>
+     *
+     * @param xOffset x offset to be moved.
+     * @param yOffset y offset to be moved.
+     */
+    public void move(int xOffset, int yOffset) {
+        this.coordinate.move(xOffset, yOffset);
     }
 
     public void moveSafely(Direction direction) {
@@ -55,7 +71,7 @@ public abstract class Actor {
     }
 
     public boolean peek(int xOffset, int yOffset) {
-        final Coordinate dest = this.coordinate.clone();
+        final Coordinate dest = this.coordinate.copy();
         dest.move(xOffset, yOffset);
         return peek(dest);
     }
