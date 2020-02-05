8936f7f5d9142a75112e4c8122db8ba5558dc1ba
kuro <dirkreher@ymail.com>
2020-02-05 11:15:04 +0100

    added GhostAI framework

--- src/main/java/game/logic/actor/ghostAI/GhostAI.java
+++ src/main/java/game/logic/actor/ghostAI/GhostAI.java
@@ -1,4 +1,9 @@
 package game.logic.actor.ghostAI;
 
+import game.logic.actor.Ghost;
+import game.logic.actor.Pac;
+import game.logic.area.position.Direction;
+
 public interface GhostAI {
+    Direction getTargetDirection(Ghost ghost, Pac pac);
 }
--- src/main/java/game/logic/actor/ghostAI/StandardAI.java
+++ src/main/java/game/logic/actor/ghostAI/StandardAI.java
@@ -1,4 +1,14 @@
 package game.logic.actor.ghostAI;
 
-public class StandardAI {
+import game.logic.actor.Ghost;
+import game.logic.actor.Pac;
+import game.logic.area.position.Direction;
+
+public class StandardAI implements GhostAI {
+    public static final StandardAI STANDARD_AI = new StandardAI();
+
+    @Override
+    public Direction getTargetDirection(Ghost ghost, Pac pac) {
+        return null;
+    }
 }
